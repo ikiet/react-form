94e150c298452878937f7abc37b00f06aa93d61b
Kiet Cao <kietcao@tuta.io>
2024-08-03 21:20:34 +0900

    use useState to manipulate form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValue, setEnteredValue] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(enteredValue);
+  };
+
+  const onEnteredValueChange = (field, event) => {
+    setEnteredValue((currentValue) => {
+      return {
+        ...currentValue,
+        [field]: event.target.value,
+      };
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            value={enteredValue.email}
+            onChange={(event) => onEnteredValueChange("email", event)}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            value={enteredValue.password}
+            onChange={(event) => onEnteredValueChange("password", event)}
+          />
         </div>
       </div>
 
